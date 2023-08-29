59ae8947f68ad02d64ad851c0826d470b3a991a0
atf <atf@pop-os.localdomain>
2023-08-29 20:48:59 +0200

    add mouse entered signal

--- root.gd
+++ root.gd
@@ -1,9 +1,10 @@
 extends Node
 
-var GLOBAL = "global hello"
+var scene = preload("res://province.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var instance = scene.instantiate()
+	add_child(instance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
