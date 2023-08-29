5fa4b81496026645063636218e3426c1949ae5ae
atf <atf@pop-os.localdomain>
2023-08-29 21:05:11 +0200

    add proper CollisionPlygon2d from code

--- province.gd
+++ province.gd
@@ -3,7 +3,10 @@ extends Polygon2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var vertices: PackedVector2Array = self.polygon
+	var collision_polygon_2d = CollisionPolygon2D.new()
+	collision_polygon_2d.polygon = vertices
+	$Area2D.add_child(collision_polygon_2d)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +15,6 @@ func _process(delta):
 
 func _on_area_2d_mouse_entered():
 	print("Entered Area2D")
+	
+	# example uf using global variable from autoloaded singleton code
 	print(MapLoader.GLOBAL)
