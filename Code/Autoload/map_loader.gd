9c3e1b89f84b919f16235f441b1375f1de86af4d
atf <atf@pop-os.localdomain>
2023-09-18 16:36:00 +0200

    restructurize code

--- Code/Autoload/map_loader.gd
+++ Code/Autoload/map_loader.gd
@@ -7,73 +7,65 @@ var countries_JSON = {
 var countries_INTERNAL = []
 
 var province_scene = preload("res://Scenes/province.tscn")
-var provinces_DICT = {
-	"Pomerania": { "country": "Poland", "pop": 3_000_000 },
-	"Mecklenburg–WestPomerania":{ "country": "Germany", "pop": 2_000_000 }
-}
 var provinces_INTERNAL = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# serialize countries
-	for country in countries_JSON:
-		#print(country)
-#		print(countries_JSON[country]) # print key
-#		print(countries_JSON[country]["capital"]) # print value
-#		print(countries_JSON[country]["color"]) # print value
-		countries_INTERNAL.append( Country.new( country, countries_JSON[country]["capital"], countries_JSON[country]["color"] ) )
-	print("We serialized {c} countries".format({"c": countries_INTERNAL.size() }))
+#	for country in countries_JSON:
+#		#print(country)
+##		print(countries_JSON[country]) # print key
+##		print(countries_JSON[country]["capital"]) # print value
+##		print(countries_JSON[country]["color"]) # print value
+#		countries_INTERNAL.append( Country.new( country, countries_JSON[country]["capital"], countries_JSON[country]["color"] ) )
+#	print("We serialized {c} countries".format({"c": countries_INTERNAL.size() }))
 	
-	# serialize provinces
+	
+	# FOR IMPORTER
+	# deserialize provinces
 	var data = _deserialize_provinces("provincesExported.json")
-	print("Serialized exported from eidtor provinces are: ")
 	for i in data:
-		print(i.shape is String)
+#		print(i.shape is String)
 		var province_instance = province_scene.instantiate()
 		province_instance.province_name = i.name
 		province_instance.shape = i.shape
 		add_child(province_instance)
 	
-		
-	for province in provinces_DICT:
-		#print(province)
-		#provinces_INTERNAL.append( Province.new( provinces_JSON[province]["country"], provinces_JSON[province]["pop"] ) )
-		pass
-	#print("We serialized {p} provinces".format({"p": provinces_INTERNAL.size() }))
 	
+	# FOR EXPORTER OF PROVINCES
 	# iterate over all drew in editor provinces and make actual logical Province out of vertices
-	for i in get_node("/root/Root/WorldDrawDebug/Node2D").get_children():
-		return
-		# if it's not Polygon2D then it's some debug temp shit probablu and we don't care
-		if not (i is Polygon2D):
-			continue
-		
-		var name = i.name
-		var shape = i.polygon
-		provinces_INTERNAL.append( { "name" = name, "shape" = shape } )
-		#print("Province: {p} with coords: {c}".format({"p": i.name, "c": i.polygon}))
-		
-		var province_instance = province_scene.instantiate()
-		province_instance.province_name = i.name
-		province_instance.shape = i.polygon
-		# this if check ensures that name from godot internal provinces matches ones from external DB
-		# THERE SHOULD NEVER BE else PATH EXECUTED! if so fix the data (probablu Province name mismatch)
-		# i.name - name of Province from internal Godot map
-		# provinces_JSON.keys() - name of Province from dict DB
-		if i.name in provinces_DICT.keys():
-			print("Province: {p} is in province dictionary".format({"p": i.name}))
-			var country = provinces_DICT[i.name]["country"]
-			province_instance.country = country
-			var country_color = countries_JSON[country]["color"]
-			province_instance.color = country_color
-			var pop = provinces_DICT[i.name]["pop"]
-			province_instance.pop = pop
-		else:
-			print("### FIX IT ### Province: {p} is NOT in province dictionary".format({"p": i.name}))
-
-		add_child(province_instance)
-		
-	print("We serialized {p} provinces".format({"p": provinces_INTERNAL.size() }))
+#	for i in get_node("/root/Root/WorldDrawDebug/Node2D").get_children():
+#
+#		# if it's not Polygon2D then it's some debug temp shit probably and we don't care
+#		if not (i is Polygon2D):
+#			continue
+#
+#		var name = i.name
+#		var shape = i.polygon
+#		provinces_INTERNAL.append( { "name" = name, "shape" = shape } )
+##		print("Province: {p} with coords: {c}".format({"p": i.name, "c": i.polygon}))
+#
+##		var province_instance = province_scene.instantiate()
+##		province_instance.province_name = i.name
+##		province_instance.shape = i.polygon
+##		# this if check ensures that name from godot internal provinces matches ones from external DB
+##		# THERE SHOULD NEVER BE else PATH EXECUTED! if so fix the data (probablu Province name mismatch)
+##		# i.name - name of Province from internal Godot map
+##		# provinces_JSON.keys() - name of Province from dict DB
+##		if i.name in provinces_DICT.keys():
+##			print("Province: {p} is in province dictionary".format({"p": i.name}))
+##			var country = provinces_DICT[i.name]["country"]
+##			province_instance.country = country
+##			var country_color = countries_JSON[country]["color"]
+##			province_instance.color = country_color
+##			var pop = provinces_DICT[i.name]["pop"]
+##			province_instance.pop = pop
+##		else:
+##			print("### FIX IT ### Province: {p} is NOT in province dictionary".format({"p": i.name}))
+##
+##		add_child(province_instance)
+#
+#	print("We serialized {p} provinces".format({"p": provinces_INTERNAL.size() }))
 	
 	# iterare provinces and add to country it belongs to
 	# for prov in provinces_INTERNAL:
@@ -92,15 +84,16 @@ func _deserialize_provinces(file_name: String):
 		var vector_as_string = i.shape
 		vector_as_string = vector_as_string.substr(1, len(vector_as_string) - 2)
 		var array_of_string_vec2s = vector_as_string.split("),")
-		print(array_of_string_vec2s)
+		
+		# convert stringified list of shape vectors to PackedList
+		var shape = PackedVector2Array()
 		for vec2_str in array_of_string_vec2s:
-			print(vec2_str)
-			vec2_str = vec2_str.replace("(", "")
 			vec2_str = vec2_str.replace(" (", "")
+			vec2_str = vec2_str.replace("(", "")
 			vec2_str = vec2_str.replace(",", "")
-			vec2_str = vec2_str.strip
 			vec2_str = vec2_str.split(" ")
-			print(vec2_str)
-		i.shape = str_to_var(i.shape)
+			var vec2_int = Vector2( int(vec2_str[0]), int(vec2_str[1]) )
+			shape.append(vec2_int)
+		i.shape = shape
 	prov_file.close()
 	return data
--- Code/root.gd
+++ Code/root.gd
@@ -4,13 +4,28 @@ extends Node
 func _ready():
 	var args = Array(OS.get_cmdline_args())
 	if args.has("-exportProvinces"):
-		print("Exporting provinces...")
+		print(">>> Cmd Line Arg deteced: \"-exportProvinces\" \n>>> Exporting provinces...")
+		_prepare_provinces_to_export()
 		_export_provinces()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+	
+func _prepare_provinces_to_export():
+	for i in get_node("/root/Root/WorldDrawDebug/Node2D").get_children():
+		
+		# if it's not Polygon2D then it's some debug temp shit probably and we don't care
+		if not (i is Polygon2D):
+			continue
+		
+		var name = i.name
+		var shape = i.polygon
+		MapLoader.provinces_INTERNAL.append( { "name" = name, "shape" = shape } )
+		
+	print(">>> We collected {p} provinces".format({"p": MapLoader.provinces_INTERNAL.size() }))
 	
 func _export_provinces():
 	var prov_json = JSON.stringify(MapLoader.provinces_INTERNAL)
