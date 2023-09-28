3fa1d5e46ad492ca9911cdcca85733fcee3a01ed
atf <atf@pop-os.localdomain>
2023-09-28 22:49:59 +0200

    add lesser poland province and a lot of UI work

--- Code/ui.gd
+++ Code/ui.gd
@@ -4,7 +4,8 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SignalRelay.province_name_changed_signal.connect(_print_test)  # args from signal are passes implictly to connected func
-	$Control/MarginContainer/VBoxContainer/HBoxContainer/Province_Name_Label.add_theme_font_size_override("font_size", 40)
+	$Control/PanelContainer/MarginContainer/GridContainer/ProvinceNameLabel.add_theme_font_size_override("font_size", 40)
+	$Control/PanelContainer/MarginContainer/GridContainer/CountryNameLabel.add_theme_font_size_override("font_size", 40)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -12,4 +13,4 @@ func _process(delta):
 
 func _print_test(province_name):
 	print("Province name changed Signal received! {name}".format( {"name": province_name} ) )
-	$Control/MarginContainer/VBoxContainer/HBoxContainer/Province_Name_Label.text = province_name
+	$Control/PanelContainer/MarginContainer/GridContainer/ProvinceNameLabel.text = province_name
